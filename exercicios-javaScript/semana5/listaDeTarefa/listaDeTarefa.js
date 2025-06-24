f7f415c3f77ffe553a6857a5243d478807b4b9d5
Louise Morais <louisemorais3@gmail.com>
2025-06-24 17:07:57 -0300

    subindo atualização do  exercicio 3  da semana 5

--- exercicios-javaScript/semana5/listaDeTarefa/listaDeTarefa.js
+++ exercicios-javaScript/semana5/listaDeTarefa/listaDeTarefa.js
@@ -4,13 +4,44 @@ const lista= document.createElement('ul');
 
 botao.addEventListener("click", (event)=>{
     event.preventDefault();
-    const conteudo = input.value;
-        if(conteudo.trim()!==""){
-            const item = document.createElement("li");
-            item.textContent= conteudo;
-            lista.appendChild(item);
+    const conteudo = input.value.trim();
+        if(conteudo!==""){
+            const dados = items(conteudo);
+            lista.appendChild(dados);
             input.value="";
+            localStorage.setItem(conteudo, JSON.stringify(conteudo));
         }
 }
 )
-document.body.appendChild(lista);
+
+
+function items(conteudo){
+    const item = document.createElement("li");
+    item.textContent= conteudo;
+    item.appendChild(apagar(item,conteudo));
+    return item;
+}
+
+
+function apagar(item,conteudo){
+    const deleteButton= document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; 
+    deleteButton.style.marginLeft="10px"
+    deleteButton.classList.add("deleteButton");
+
+    deleteButton.addEventListener('click', ()=>{
+        lista.removeChild(item);
+        localStorage.removeItem(conteudo);
+    }
+    )
+    return deleteButton;
+}
+
+
+function carregar(){
+    Object.keys(localStorage).forEach(chave => {
+        const conteudo = JSON.parse(localStorage.getItem(chave));
+        const item = items(conteudo);
+        lista.appendChild(item);
+    })
+}
